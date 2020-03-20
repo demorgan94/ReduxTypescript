8ec7d2a03f84ade8c5a60e529966e1935395fb8d
David Morgan <demorgan94@gmail.com>
2020-03-20 11:53:38 -0700

    Add jest and enzyme config

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+
 import App from '../components/App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Tests', () => {
+  it('should find Header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Header').exists()).toBe(true);
+  });
+
+  it('should find ToDoForm', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('ToDoForm').exists()).toBe(true);
+  })
 });
